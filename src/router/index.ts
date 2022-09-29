15335b76dcd43b403529795b9fb607b1edfffd7b
Hersan Kuang <mlnrmsw@gmail.com>
2022-09-30 00:15:09 +0800

    build: create and initialize the trip project

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  // 映射关系：path -> components
+  routes: [
+
+  ]
+})
+
+export default router
---
+++ src/views/message/message.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="message">
+    <h2>message</h2>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
