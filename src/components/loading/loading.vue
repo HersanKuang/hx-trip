f362bc9ca87ea2eb1f5b262be8e23ffbc11b9816
Hersan Kuang <mlnrmsw@gmail.com>
2022-10-25 22:39:09 +0800

    refactor: build loading animation

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -5,46 +5,40 @@ const loadingStore = useLoadingStore()
 </script>
 
 <template>
-  <div v-if="loadingStore.isLoading" class="global-loading">
-    <div class="loading-bg"></div>
-    <div class="loading"></div>
+  <div v-if="loadingStore.isLoading" class="loading">
+    <div class="bg">
+      <img src="@/assets/img/home/full-screen-loading.gif" alt="" />
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-.global-loading {
+.loading {
   position: fixed;
-  top: 50%;
-  left: 50%;
-  width: 104px;
-  height: 104px;
-  margin-left: -52px;
-  margin-top: -52px;
-  z-index: 2000;
+  z-index: 999;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  .loading-bg {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-image: url(https://fe.tujiacdn.com/pwa/static/fevueui/images/loading/loading-bg.png);
-    background-size: 104px 104px;
-    background-repeat: no-repeat;
-    background-position: 50%;
-  }
+  background-color: rgba(0, 0, 0, 0.2);
 
-  .loading {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    margin: auto;
-    width: 70px;
-    height: 70px;
-    background-image: url(@/assets/img/home/full-screen-loading.gif);
-    background-size: 70px 70px;
-    background-repeat: no-repeat;
-    background-position: 50%;
+  .bg {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 104px;
+    height: 104px;
+    background: url(@/assets/img/home/loading-bg.png) 0 0 / 100% 100%;
+
+    img {
+      width: 70px;
+      height: 70px;
+      margin-bottom: 8px;
+    }
   }
 }
 </style>
