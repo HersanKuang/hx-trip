f2044e4310b413b81a00b730d23939c63f399d5e
Hersan Kuang <mlnrmsw@gmail.com>
2022-11-04 04:05:12 +0800

    feat: the message page is set up

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -1,9 +1,35 @@
-<script lang="ts" setup></script>
-
 <template>
   <div class="message">
-    <h2>message</h2>
+    <van-nav-bar
+      title="消息中心"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+    <div class="content">
+      <img src="@/assets/img/message/icon_message.png" alt="" />
+      <div class="text">暂无聊天消息</div>
+    </div>
   </div>
 </template>
 
-<style lang="less" scoped></style>
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const onClickLeft = () => {
+  router.back()
+}
+</script>
+
+<style lang="less" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+  img {
+    width: 88%;
+  }
+}
+</style>
